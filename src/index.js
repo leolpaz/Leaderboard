e0b2f31f0a787eee44777bc74334a2c02c36211e
leolpaz <leonardolpaz95@gmail.com>
2021-10-05 17:59:57 -0300

    Add form functionality to add new scores to the table

--- src/index.js
+++ src/index.js
@@ -1,2 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+
+class Score {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+}
+
+class ScoreArray {
+  static returnArray(){
+    const scoreArray = JSON.parse(localStorage.getItem('scoreArray') || '[]');
+    return scoreArray;
+  }
+
+  static saveScore(score){
+    const scoreArray = ScoreArray.returnArray()
+    scoreArray.push(score)
+    localStorage.setItem('scoreArray', JSON.stringify(scoreArray));
+  }
+}
+
+function submitScore(name, score) {
+  const table = document.getElementById('table-body');
+  const scoreElement = document.createElement('tr')
+  scoreElement.innerHTML = `<td>${name}: ${score}</td>`
+  table.appendChild(scoreElement)
+}
+
+
+function submitListener() {
+  const submit = document.getElementById('submit-form');
+  console.log(submit)
+  submit.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const name = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    const newScore = new Score(name, score);
+    submit.reset();
+    ScoreArray.saveScore(newScore);
+    submitScore(name, score);
+  })
+}
+
+function listOnLoad() {
+  ScoreArray.returnArray().forEach((element) => {
+    submitScore(element.name, element.score)
+  })
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  submitListener()
+  listOnLoad()
+})
