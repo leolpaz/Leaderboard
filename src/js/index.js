e9b730653ee5ed634fcfc4341abb19375fb6b3b3
leolpaz <leonardolpaz95@gmail.com>
2021-10-06 14:17:30 -0300

    Create function to get and add things to the API

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
-import Score from './score.js';
 
-class ScoreArray {
-  static returnArray() {
-    const scoreArray = JSON.parse(localStorage.getItem('scoreArray') || '[]');
-    return scoreArray;
-  }
-
-  static saveScore(score) {
-    const scoreArray = ScoreArray.returnArray();
-    scoreArray.push(score);
-    localStorage.setItem('scoreArray', JSON.stringify(scoreArray));
-  }
+async function scoreArray() {
+  let response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rvyYy9iLp76dZi185ZOZ/scores/', {
+    method:'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+});
+  return await response.json();
 }
 
-function submitScore(name, score) {
+
+
+function addScore(name, score) {
   const table = document.getElementById('table-body');
   const scoreElement = document.createElement('tr');
   scoreElement.innerHTML = `<td>${name}: ${score}</td>`;
   table.appendChild(scoreElement);
+}
+
+async function submitScore(name, score) {
+ let response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rvyYy9iLp76dZi185ZOZ/scores/', {
+    method:'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({
+        "user": `${name}`,
+        "score": `${score}`   
+    })
+})
+if (!response.ok) {
+  throw new Error("Your submission did not reach the server")
+} else {
+  addScore(name, score)
+}
 }
 
 function submitListener() {
@@ -28,30 +46,42 @@ function submitListener() {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const score = document.getElementById('score').value;
-    const newScore = new Score(name, score);
     submit.reset();
-    ScoreArray.saveScore(newScore);
     submitScore(name, score);
   });
 }
 
-function listOnLoad() {
-  ScoreArray.returnArray().forEach((element) => {
-    submitScore(element.name, element.score);
+async function listOnLoad() {
+  const table = document.getElementById('submit-form');
+  table.innerHTML = ""
+  let sArray = [];
+  await scoreArray().then((scores) => {sArray = scores.result})
+  console.log(sArray)
+  sArray.forEach((element) => {
+    addScore(element.user, element.score);
   });
 }
 
-function refresh() {
-  const refreshBtn = document.getElementById('refresh');
-  refreshBtn.addEventListener('click', () => {
-    const table = document.getElementById('table-body');
-    table.innerHTML = '';
-    listOnLoad();
-  });
-}
+// function refresh(isButton) {
+//   if(isButton === "refresh"){
+//     console.log('called')
+//     function callLoad(){
+//     const table = document.getElementById('table-body');
+//     table.innerHTML = '';
+//     listOnLoad();
+//   }
+//   refreshBtn.addEventListener('click', callLoad());}
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   submitListener();
-  listOnLoad();
-  refresh();
+  listOnLoad()
 });
+
+document.addEventListener('click', (event) => {
+  const isButton = event.target.id
+  if (isButton === 'refresh'){
+    listOnLoad()
+    console.log('called')
+  }
+})
